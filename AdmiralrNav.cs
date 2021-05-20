6696342da50cdfde0ff8f5fa7a085425e9aab16f
AdmiralGhost <AdmiralGhost365@Gmail.com>
2021-05-20 17:11:32 -0400

    Add files via upload

---
+++ AdmiralrNav.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AdmiralrNav : MonoBehaviour
+{
+
+    NavMeshAgent navi;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navi = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            navi.SetDestination(hit.point);
+        }
+    }
+}
